65421e8ff55f59b0c3c8c8f96dc0efa2be1adae7
Jaggwagg <33637354+Jaggwagg@users.noreply.github.com>
2025-06-01 12:12:09 -0500

    Formatting

--- fabric/src/main/java/jaggwagg/template/platform/FabricPlatformHelper.java
+++ fabric/src/main/java/jaggwagg/template/platform/FabricPlatformHelper.java
@@ -4,7 +4,6 @@ import jaggwagg.template.platform.services.IPlatformHelper;
 import net.fabricmc.loader.api.FabricLoader;
 
 public class FabricPlatformHelper implements IPlatformHelper {
-
     @Override
     public String getPlatformName() {
         return "Fabric";
